4f1ac315f4ae3644509e203a1b54fb52e76cd329
Terynrose <63327501+Terynrose@users.noreply.github.com>
2020-10-20 17:08:44 -0400

    svg push

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -3,10 +3,12 @@ import {
 } from "gsap";
 
 const lampTL = gsap.timeline();
-lampTL.from("#WL", {
-    duration: 0.5,
-    y: -300
-});
+lampTL.to("#Shine", {duration: 0.05, alpha: 0})
+    .from("#Lamp-White", {duration: 0.5, y: -300})
+    .to("#Switch", {duration: 0.25, scaleY: 1.5})
+    .to("#Switch", {duration: 0.25, scaleY: 1})
+    .to("#white", {duration: 0.5, backgroundColor: "$white"});
+
 
 export function lampAnimation() {
     return lampTL;
