d39c64db7eef60a094f470e77700427d73ebe722
Terynrose <63327501+Terynrose@users.noreply.github.com>
2020-11-22 18:09:12 -0500

    josh push

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -1,7 +1,12 @@
 import {gsap} from "gsap";
+
+import $ from "jquery"
 // import{DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 
 // gsap.registerPlugin(DrawSVGPlugin);
+//var logoHeight = $("#logo").height();
+
+//console.log(logoHeight  + ' is the logo height');
 
 
 const logoTL = gsap.timeline();
@@ -11,7 +16,7 @@ logoTL.from("#Body", {duration: 1, alpha: 0}, "second")
         .from("#Beak", {duration: 1, alpha: 0}, "after-third")
         .from("#Shadows", {duration: 1, alpha: 0}, "after-third")
         .from("#Feathers", {delay: 1.5, duration: 1, alpha: 0}, "third")
-        .to("#logo", {duration: 1, y: 210, scale: 1.25}, "fourth")
+        .to("#logo", {duration: 1, y: 200, scale: 1.25}, "fourth")
         .to("#LW", {duration: 1, alpha: 0}, "fourth")
         .to("#RW", {duration: 1, alpha: 0}, "fourth")
         .to("#Beak", {duration: 1, alpha: 0}, "fourth")
